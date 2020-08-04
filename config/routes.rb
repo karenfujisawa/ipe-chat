0b418a12e0ffc3f42f486a8e9fa54f190b0d5811
karenfujisawa <karen.fujisawa@ipeinc.jp>
2020-08-04 22:58:45 +0900

    第7章

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
   get "messages/index", to: "messages#index"
-  get "", to: "messages#index"
+  get "", to: "messages#index" , as: "root"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
